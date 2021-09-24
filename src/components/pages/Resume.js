44a29d79ce33e706d1e68f8b0cf581fd45192eda
grace butler <gmx.butler@gmail.com>
2021-09-24 13:46:52 -0500

    was able to figure out how to use font awesome without importing the packages, added script to index.html, and favicons to the resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -9,9 +9,22 @@ function Resume(props) {
       <section className= "contactpg bg-green pt-5 text-black">
       <div className= "text-center">
         <h1 className= "text-7xl m-2"> Resume </h1>
-        <a href= {downloadresume} className= "text-2xl">Download<img src= {downloadimg} alt="download-img"style={{height: '20px', width:'20px', display:'block', margin:'auto'}}></img></a> 
-        <img src= {pg1} alt="resume1" style={{display:'block', margin:'auto', paddingBottom: '5px', paddingTop: '10px'}}></img>
-        <img src= {pg2} alt="resume2" style={{display:'block', margin:'auto',paddingBottom: '10px'}}></img> 
+        <br></br>
+          <div className= "space-x-10">
+          <i class="fab fa-html5 fa-3x"></i>
+          <i class="fab fa-css3 fa-3x"></i>
+          <i class="fab fa-js-square fa-3x"></i>
+          <i class="fab fa-node-js fa-3x"></i>
+          <i class="fab fa-react fa-3x"></i>
+          <i class="fab fa-npm fa-3x"></i>
+
+          </div>
+          <br></br>
+          <div>
+          <a href= {downloadresume} className= "text-2xl">Download<img src= {downloadimg} alt="download-img"style={{height: '20px', width:'20px', display:'block', margin:'auto'}}></img></a> 
+          <img src= {pg1} alt="resume1" style={{display:'block', margin:'auto', paddingBottom: '5px', paddingTop: '10px'}}></img>
+          <img src= {pg2} alt="resume2" style={{display:'block', margin:'auto',paddingBottom: '10px'}}></img> 
+          </div> 
       </div>
       </section>
     );
