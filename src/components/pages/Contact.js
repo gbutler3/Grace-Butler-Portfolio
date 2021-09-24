4858d71ba083d77149ab79852508065e1ff3e69f
grace butler <gmx.butler@gmail.com>
2021-09-24 12:37:51 -0500

    form displaying correctly, and alerts working

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 import github from "../../assets/logos/github-logo.png";
 import linkedin from "../../assets/logos/linkedin-logo.svg";
 import mail from "../../assets/logos/mail-closed-envelope-back.svg";
 import phone from "../../assets/logos/phone-receiver-silhouette.svg";
+import { validateEmail } from '../utils/helpers';
+
 
 function Contact() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [submitname, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    }
+
+    if (inputType === 'Name') {
+      setName(inputValue);
+    }
+    if (inputType === 'message') {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();    
+
+    // First we check to see if the email is not valid or if the email is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email)) {
+      setErrorMessage('Please enter a valid email!');
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setEmail('');
+    setName('');
+    setMessage('');
+  };
   return (
     <section className= "contactpg bg-green pt-5">
       <div className= "text-center">
@@ -25,7 +69,37 @@ function Contact() {
         </ul>
       </div>
 
-      
+      <form className="form flex items-center justify-center ">
+        <input className= "m-2"
+            value={submitname}
+            name="Name"
+            onChange={handleInputChange}
+            type="Name"
+            placeholder="Name"
+        />
+        <br></br>
+        <input className= "m-2"
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <br></br>
+        <input className= "m-2"
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            type="message"
+            placeholder="Message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text text-center text-pink font-semibold">{errorMessage}</p>
+        </div>
+      )}
     </section>
   );
 }
